15f93a9732dcb79d3ebfc14437afdb62e0f0e5fa
guiolivi <guilhermeoli.dev@gmail.com>
2026-05-20 17:34:37 -0300

    General fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int outputPin;
+unsigned long pulse, deltaT;
+const int diamWheel = 22;
+const float kmIn = (100UL*1000)/2.54, hrMs = 1000UL*60*60, inMs = kmIn/hrMs, perimeter = diamWheel*PI;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(2, INPUT);
+  Serial.begin(9600);
+  Serial.print("In/ms: " + String(inMs) + "\nKilometers to inches: " + String(kmIn) + "\nHours to milliseconds: " + String(hrMs));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (outputPin == LOW && outputPin != digitalRead(2)) {
+    deltaT = millis() - pulse;
+    pulse = millis();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (deltaT == 0) {
+      return;
+    } else {
+      Serial.println((1.0/deltaT)*inMs*perimeter, 10);
+    }
+  }
+  
+  outputPin = digitalRead(2);
 }
